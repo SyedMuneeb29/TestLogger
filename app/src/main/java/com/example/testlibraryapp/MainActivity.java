df07fcedf440dfd623356f722e6f0554e1d44976
muneeb.rehman <muneeb.rehman@avanza.pk>
2020-08-16 20:16:04 +0500

    test 2 added

--- app/src/main/java/com/example/testlibraryapp/MainActivity.java
+++ app/src/main/java/com/example/testlibraryapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.example.testlibrary.TestClass;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ test2Library/src/main/java/com/example/test2library/Test2Class.java
@@ -0,0 +1,11 @@
+package com.example.test2library;
+
+import android.util.Log;
+
+public class Test2Class {
+
+    public static void LogIt () {
+        Log.d("" , "" ) ;
+    }
+
+}
